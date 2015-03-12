8a598b4ce1b1424165febedb233d6326e7ccb7d8
Wang Fei <wangfei-vince@outlook.com>
2015-03-12 17:56:41 +0800

    将服务器做下载代理

--- download_proxy_on_linux_for_windows.py
+++ download_proxy_on_linux_for_windows.py
@@ -1,7 +1,9 @@
 import paramiko
 import os
 # running on Windows env
-# use pem to login ec2 instance from windows desktop
+# linux server downloads object faster than Windows Desktop
+# and make sure bandwidth between windows and linux server is nice
+# here I use private linux server in IDC with direct connect to Office
 
 #download information
 object_url = r"http://dldir1.qq.com/music/clntupate/QQMusic_Setup_1152.exe"
@@ -24,10 +26,9 @@ ssh.close()
 t = paramiko.Transport((hostname, 22))
 t.connect(username=username,password=password)
 sftp = paramiko.SFTPClient.from_transport(t)
-
+# store path and download path
 remote_path = '/home/' + username + '/' + object_name
 local_path = r"E:\Download" + os.sep + object_name
-
 print "Now download to local desktop from remote linux"
 sftp.get(remote_path, local_path)
 t.close
